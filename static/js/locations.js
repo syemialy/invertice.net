de29b7e264d46db70a797dec8e204de11e2f3ccf
Sergey Emelianov <semelianov@gmail.com>
2017-01-31 13:55:22 +0300

    presentation slides converted

---
+++ static/js/locations.js
@@ -0,0 +1,67 @@
+$(function($) {
+    // Asynchronously Load the map API 
+    var script = document.createElement('script');
+    script.src = "//maps.googleapis.com/maps/api/js?sensor=false&callback=initialize";
+    document.body.appendChild(script);
+});
+
+function initialize() {
+    var map;
+    var bounds = new google.maps.LatLngBounds();
+
+    var mapOptions = {
+        mapTypeId: 'roadmap'
+    };
+                    
+    // Display a map on the page
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    map.setTilt(45);
+        
+    // Multiple Markers
+    var markers = [
+        ['Invertice Corporate Office & Warehouse, Philadelphia PA', 40.2474552,-75.1995507],
+        ['Invertice Sattellite Operations, Pitsburg',40.431478,-80.0505406]
+    ];
+                        
+    // Info Window Content
+    var infoWindowContent = [
+        ['<div class="info_content">' +
+        '<h3>Invertice Corporate Office & Warehouse</h3>' +
+        '<p>Philadelphia Pa, NJ, NE, East, NYC, Upstate, BAWA and More!</p></div>'],
+        ['<div class="info_content">' +
+        '<h3>Invertice Sattellite Operations</h3>' +
+        '<p>Pittsburgh Pa – OHPA, Central,And Mid Atlantic Service Area</p></div>']
+    ];
+        
+    // Display multiple markers on a map
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+    
+    // Loop through our array of markers & place each one on the map  
+    for( i = 0; i < markers.length; i++ ) {
+        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+        bounds.extend(position);
+        marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: markers[i][0]
+        });
+        
+        // Allow each marker to have an info window    
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infoWindow.setContent(infoWindowContent[i][0]);
+                infoWindow.open(map, marker);
+            }
+        })(marker, i));
+
+        // Automatically center the map fitting all markers on the screen
+        map.fitBounds(bounds);
+    }
+
+    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+        this.setZoom(14);
+        google.maps.event.removeListener(boundsListener);
+    });
+    
+}
