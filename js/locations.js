0a6bb2508ce697636bd22aa18229042c654c2251
Sergey Emelianov <semelianov@gmail.com>
2017-02-04 17:48:37 +0300

    pushing new look to invertice.net site

--- js/locations.js
+++ js/locations.js
@@ -1,67 +1,48 @@
 $(function($) {
     // Asynchronously Load the map API 
     var script = document.createElement('script');
-    script.src = "//maps.googleapis.com/maps/api/js?key=AIzaSyBsJ1zFGXY4DVMLeY_5srACRnGkXCW02y8&callback=initialize";
+    script.src = "//maps.google.com/maps/api/js?key=AIzaSyBsJ1zFGXY4DVMLeY_5srACRnGkXCW02y8&callback=initialize";
     document.body.appendChild(script);
 });
 
 function initialize() {
-    var map;
-    var bounds = new google.maps.LatLngBounds();
-
-    var mapOptions = {
-        mapTypeId: 'roadmap'
-    };
-                    
-    // Display a map on the page
-    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-    map.setTilt(45);
-        
+  
     // Multiple Markers
-    var markers = [
+    var locations = [
         ['Invertice Corporate Office & Warehouse, Philadelphia PA', 40.2474552 ,-75.1995507],
         ['Invertice Sattellite Operations, Pitsburg',40.431478,-80.0505406]
     ];
-                        
-    // Info Window Content
-    var infoWindowContent = [
-        ['<div class="info_content">' +
-        '<h3>Invertice Corporate Office & Warehouse</h3>' +
-        '<p>Philadelphia Pa, NJ, NE, East, NYC, Upstate, BAWA and More!</p></div>'],
-        ['<div class="info_content">' +
-        '<h3>Invertice Sattellite Operations</h3>' +
-        '<p>Pittsburgh Pa – OHPA, Central,And Mid Atlantic Service Area</p></div>']
+   /* 
+     var locations = [
+      ['Bondi Beach', -33.890542, 151.274856, 4],
+      ['Coogee Beach', -33.923036, 151.259052, 5],
+      ['Cronulla Beach', -34.028249, 151.157507, 3],
+      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+      ['Maroubra Beach', -33.950198, 151.259302, 1]
     ];
-        
-    // Display multiple markers on a map
-    var infoWindow = new google.maps.InfoWindow(), marker, i;
-    
-    // Loop through our array of markers & place each one on the map  
-    for( i = 0; i < markers.length; i++ ) {
-        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
-        bounds.extend(position);
-        marker = new google.maps.Marker({
-            position: position,
-            map: map,
-            title: markers[i][0]
-        });
-        
-        // Allow each marker to have an info window    
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-                infoWindow.setContent(infoWindowContent[i][0]);
-                infoWindow.open(map, marker);
-            }
-        })(marker, i));
-
-        // Automatically center the map fitting all markers on the screen
-        map.fitBounds(bounds);
-    }
-
-    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
-    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-        this.setZoom(14);
-        google.maps.event.removeListener(boundsListener);
+    */
+    var map = new google.maps.Map(document.getElementById('map_canvas'), {
+      zoom: 7,
+      center: new google.maps.LatLng(40.431478,-80.0505406),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
     });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
     
 }
